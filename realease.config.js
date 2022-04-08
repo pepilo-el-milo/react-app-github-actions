96e67b3ef22202c2d2c8bbf1db6300f2ba89f9c4
Pepe Gomez <pepegomez2001@hotmail.com>
2022-04-08 17:44:54 -0500

    fix: fix description

---
+++ realease.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/pepilo-el-milo/react-app-github-actions.git",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+    ]
+}
